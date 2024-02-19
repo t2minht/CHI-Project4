40627def039dda137e2daf3036ddd1390acb55cf
t2minht <tuongtran0240@gmail.com>
2024-02-19 11:52:56 -0600

    setup

---
+++ app.js
@@ -0,0 +1,22 @@
+// const express = require('express')
+// const app = express()
+// const port = 1001
+
+// app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'))
+// app.listen(port, () => console.log("app on port ${port}"))
+// app.get(/^(.+)$/, function(req,res){
+//     console.log('static file require: ' +req.params);
+//     res.sendFile(__dirname + req.params[0]);
+// });
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => res.sendfile(__dirname + '/index.html'))
+app.listen(port, () => console.log(`app on port ${port}!`))
+
+app.get(/^(.+)$/, function(req, res){ 
+	console.log('static file require: ' + req.params);
+	res.sendFile(__dirname + req.params[0]);
+});
