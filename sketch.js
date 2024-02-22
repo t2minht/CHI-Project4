58646a35429137f832f53158398bd0988f964cd8
t2minht <tuongtran0240@gmail.com>
2024-02-21 18:05:29 -0600

    picture pos finished

--- sketch.js
+++ sketch.js
@@ -1,11 +1,14 @@
 let button, img, p;
 let imgs = ['01_bulbasaur.png', '02_ivysaur.png', '03_venusaur.png', '06_charizard.png', '07_squirtle.png', '08_wartortle.png', '09_blastoise.png', '10_caterpie.png', '11_metapod.png', '12_butterfree.png', '13_weedle.png', '14_kakuna.png', '15_beedrill.png', '16_pidgey.png', '17_pidgeotto.png', '18_pidgeot.png', '19_rattata.png', '20_raticate.png'];
+let crys = ['01_bulbasaur.mp3', '02_ivysaur.mp3', '03_venusaur.mp3', '06_charizard.mp3', '07_squirtle.mp3', '08_wartortle.mp3', '09_blastoise.mp3', '10_caterpie.mp3', '11_metapod.mp3', '12_butterfree.mp3', '13_weedle.mp3', '14_kakuna.mp3', '15_beedrill.mp3', '16_pidgey.mp3', '17_pidgeotto.mp3', '18_pidgeot.mp3', '19_rattata.mp3', '20_raticate.mp3'];
 let pokemon = []
 let pokeButtons = []
 let currentPokemon = -1
+let sizeVal = 64
+let slider;
 
 function buttonClick(){
-  alert(this.value());
+  // alert(this.value());
   currentPokemon = this.value();
 }
 
@@ -63,7 +66,7 @@ function setup() {
   rect(800,150, 400,650,5);
   let inp = createInput('Nickname');
   inp.position(250,750);
-  let slider = createSlider(0, 100, 50);
+  slider = createSlider(0, 100, 50);
   slider.position(250,700);
   slider.size(200);
  
@@ -97,8 +100,14 @@ function draw(){
   rect(75,150, 600,650,5);
   fill(0,0,0);
   textSize(15);
+  // alert(slider.value());
+  sizeVal = map(slider.value(), 0, 100, 64, 640);
+  x = 375
+  y = 450
   if(currentPokemon != -1){
-    image(pokemon[int(currentPokemon)], 300, 350);
+    x -= sizeVal /2;
+    y -= sizeVal /2;
+    image(pokemon[int(currentPokemon)], x, y, sizeVal, sizeVal);
   }
   text('Click to pet and play sound',250,200);
   text('size',225,720);
